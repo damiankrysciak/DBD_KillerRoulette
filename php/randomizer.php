72250ebbe35f8bfe1cace4a515260a33eb25276d
damian <dkrysciak99@gmail.com>
2025-01-24 16:47:41 +0100

    Add files via upload
    Few minor tweaks & changes

--- php/randomizer.php
+++ php/randomizer.php
@@ -3,14 +3,16 @@
 $selected_options = $_POST['auswahl'] ?? [];
 
 $killers = array("Trapper", "Wraith", "Hillbilly", "Nurse", "Shape", "Hag", "Doctor", "Huntress", "Cannibal",
-                "Nightmare", "Pig", "Clown", "Spirit", "Legion", "Plague", "Ghost_Face", "Demogorgon", "Oni",
+                "Nightmare", "Pig", "Clown", "Spirit", "Legion", "Plague", "Ghost Face", "Demogorgon", "Oni",
                 "Deathslinger", "Executioner", "Blight", "Twins", "Trickster", "Nemesis", "Cenobite", "Artist",
-                "Onryo", "Dredge", "Mastermind", "Knight", "Skull_Merchant", "Singularity", "Xenomorph", "Good_Guy",
-                "Unknown", "Lich", "Dark_Lord", "Houndmaster");
+                "Onryo", "Dredge", "Mastermind", "Knight", "Skull Merchant", "Singularity", "Xenomorph", "Good Guy",
+                "Unknown", "Lich", "Dark Lord", "Houndmaster");
 
 $remaining_killers = array_diff($killers, $selected_options);
 
 $random_killer = array_rand($remaining_killers, 1);
 $selected_random_killer = $remaining_killers[$random_killer];
 
-echo '<img src="img/' . $selected_random_killer . '.png" alt="Zufälliges Bild">';
+echo '<br><div class="text-center"><h4><img src="img/' . $selected_random_killer . '.png" alt="Zufälliges Bild">';
+echo '<br><br>The ';
+echo $selected_random_killer . '</h4></div>';
